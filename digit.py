834b613f0283907150c39b5445cb2cde4002083c
Barbara Martin <bmartin@bigml.com>
2017-06-26 10:59:55 +0200

    Digit Recognition data

---
+++ digit.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  6 17:46:05 2017
+
+@author: Babishula
+"""
+
+    # This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+from keras.models import Sequential
+from keras.layers import Dense , Dropout , Lambda, Flatten
+from keras.optimizers import Adam ,RMSprop
+from sklearn.model_selection import train_test_split
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+
+# Any results you write to the current directory are saved as output.
+
+import keras
+import pip
+package_name='keras'
+pip.main(['install', package_name])
